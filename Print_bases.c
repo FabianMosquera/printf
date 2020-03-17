fd1e2440d95e5da19ae65b72aad11b2081ce06bd
Andres <anhujuju1gmai.com>
2020-03-17 20:00:45 +0000

    add Print_x

--- Print_bases.c
+++ Print_bases.c
@@ -82,7 +82,7 @@ int print_X(va_list X)
 {
     char hex [16];
     unsigned int n = 0, count = 0;
-    int i;
+    int i = 0;
 
 n = va_arg(X,unsigned int);
 if (n == 0)
@@ -95,6 +95,45 @@ if (n == 0)
        if (n % 16 >= 10 && n % 16 <= 15)
        {
            hex[i] = 55 + n % 16;
+       }
+       else
+       {
+           hex[i] = 48 + n % 16;
+       }
+       n = n/16;
+       i++;
+   }
+   for (--i; i >= 0; i--)
+    {
+        _putchar(hex[i]);
+        count++;
+    } 
+return(count);
+}
+
+/**
+* print_x - prints input integer to an hexadecimal in lowercase
+* @X: the list of arguments
+* Return: number of printed characters
+*/
+
+int print_x(va_list x)
+{
+    char hex [16];
+    unsigned int n = 0, count = 0;
+    int i = 0;
+
+n = va_arg(x,unsigned int);
+if (n == 0)
+{
+    _putchar( 0 + '0');
+    count ++;
+}
+   while (n > 0)
+   {
+       if (n % 16 >= 10 && n % 16 <= 15)
+       {
+           hex[i] = 87 + n % 16;
        }
        else
        {
